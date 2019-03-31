08fc8b98f273f902be97a7f84ea39e8914bbd45f
yaozi <bbq_yaozi@163.com>
2019-04-01 00:43:47 +0800

    后台管理-仪表板

--- sp-sys/src/main/java/com/kailash/land/mapper/ProjectMapper.java
+++ sp-sys/src/main/java/com/kailash/land/mapper/ProjectMapper.java
@@ -1,5 +1,6 @@
 package com.kailash.land.mapper;
 
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -14,5 +15,17 @@ import com.kailash.land.filter.ProjectFiter;
 public interface ProjectMapper extends BaseMapper<Project> {
 
 	Page<Map<String, Object>> querySimpleList(@Param("pro") ProjectFiter filter);
+	
+	/**
+	 * 项目统计
+	 * @return
+	 */
+	Map<String,String> projectStatistics();
+	
+	/**
+	 * 项目明细统计
+	 * @return
+	 */
+	List<Map<String,String>> projectDetailStatistics();
 
 }
--- sp-sys/src/main/java/com/kailash/land/mapper/UsersMapper.java
+++ sp-sys/src/main/java/com/kailash/land/mapper/UsersMapper.java
@@ -4,6 +4,9 @@ import com.baomidou.mybatisplus.annotations.SqlParser;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.kailash.land.entity.Users;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public interface UsersMapper extends BaseMapper<Users> {
 
@@ -18,9 +21,10 @@ public interface UsersMapper extends BaseMapper<Users> {
 	 */
 	@SqlParser(filter = true)
 	Users findByUserName(String username);
-
 	
-
-
-
+	/**
+	 * 注册会员统计
+	 * @return
+	 */
+	Map<String, String> userStatistics();
 }
